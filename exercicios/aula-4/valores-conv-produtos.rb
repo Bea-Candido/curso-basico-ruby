48d5815a2960df1f0c05bdb0178b063af6496fad
BeaPc <developer.beacandido@gmail.com>
2023-02-05 14:56:18 -0300

    feat: novo exercício

---
+++ exercicios/aula-4/valores-conv-produtos.rb
@@ -0,0 +1,14 @@
+dolar = 5
+euro = 6
+
+cadeira = 10 
+mesa = 20 
+
+mesa_euro = mesa * euro
+mesa_dolar = mesa * dolar
+
+cadeira_euro = cadeira * euro
+cadeira_dolar = cadeira * dolar
+
+puts "O preco da mesa em euros é #{mesa_euro} e o preço dela em dolar é #{mesa_dolar}"
+puts "O preco da cadeira em euros é #{cadeira_euro} e o preço dela em dolar é #{cadeira_dolar}"
