cdf4d843bfed7f3d5d5520a90cb086f9621399b4
BeaPc <developer.beacandido@gmail.com>
2023-02-05 14:51:52 -0300

    feat: nova aula

---
+++ aula-4/operacoes-matematicas.rb
@@ -0,0 +1,19 @@
+puts 2*2
+puts 3/3
+puts 1+1
+puts 2-2
+puts (100*5) / 100 + 2
+
+
+taxa = 3
+preco = 30
+
+valor  = preco +  taxa
+
+puts "O produto custa #{valor}"
+
+# ou pode fazer :
+
+puts "O produto custa #{preco + taxa}"
+
+
