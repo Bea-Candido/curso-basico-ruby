410e864e4519c19f789a66b694701d91b2d0805c
BeaPc <developer.beacandido@gmail.com>
2023-02-06 23:49:27 -0300

    feat: complementando a aula

---
+++ aula-5/arrays-e-string.rb
@@ -0,0 +1,9 @@
+nomes = "Rafael Roberto Lucas"
+
+lista_de_nomes = nomes.split(" ")
+
+puts lista_de_nomes[1]
+
+
+#Aqui iremos dividir uma string usando a função split , que irá criar uma lista ordenada 
+# O split usa o espaço entre as palavras para fazer a divisão
--- aula-5/arrays.rb
+++ aula-5/arrays.rb
@@ -1,32 +1,59 @@
 [] #array vazio
 
-nomes = ["Rafael","Lucas","Roberto"]
-#         0 / -3   1 / -2   2 / - 1
+# Exemplo 1
 
-numeros = [1,2,3,4,5]
-#         0,1,2,3,4
+# nomes = ["Rafael","Lucas","Roberto"]
+# #         0 / -3   1 / -2   2 / - 1
 
-puts "PONTUACAO"
-puts "#{nomes[0]} - #{numeros[2]}"
+# numeros = [1,2,3,4,5]
+# #         0,1,2,3,4
 
-puts "#{nomes}"
+# puts "PONTUACAO"
+# puts "#{nomes[0]} - #{numeros[2]}"
 
-puts nomes[0]
-puts nomes[1]
-puts nomes[2]
+# puts "#{nomes}"
 
-puts nomes[-1]
+# puts nomes[0]
+# puts nomes[1]
+# puts nomes[2]
 
+# puts nomes[-1]
+ 
 
-exemplo = [1,2,[4,5],6]
-#          0 1   2   3
+# exemplo = [1,2,[4,5],6,3]
+# #          0 1   2   3
 
-puts exemplo[0]
-puts exemplo[1]
-puts exemplo[3]
+# puts "#{exemplo[0]}" #1
+# puts "#{exemplo[1]}" #2
+# puts "#{exemplo[4]}" #3
+# puts "#{exemplo[2][0]}" #4
+# puts "#{exemplo[2][1]}" #5
+# puts exemplo[3] #6
 
-puts exemplo[2]
+#  Exemplo 2
+
+exemplo = [
+  "Rafael",
+  "Roberto",
+  [
+    100,
+    200,
+    300,
+    [
+      1000,
+      2000
+    ]
+  ]
+]
+
+puts exemplo[0] #Rafael
+puts "#{exemplo[2][3][0]}" # para acessar a posição  1000 na lista
 
 
 #Como é uma lista ordenada conseguimos acessar ela
 #Com o ruby funciona valores negativos , quando se coloca -1 por exemplo ele entende que você quer o último elemento da lista .
+# Você não precisa escrever a lista na mesma linha, pode ser como exemplo = {
+# 1,
+# 2,
+# 3,
+#}
