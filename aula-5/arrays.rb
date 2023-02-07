cf7ae043af68c2598f6fc476faeabe13cf47db3d
BeaPc <developer.beacandido@gmail.com>
2023-02-06 22:03:37 -0300

    feat: nova aula

---
+++ aula-5/arrays.rb
@@ -0,0 +1,32 @@
+[] #array vazio
+
+nomes = ["Rafael","Lucas","Roberto"]
+#         0 / -3   1 / -2   2 / - 1
+
+numeros = [1,2,3,4,5]
+#         0,1,2,3,4
+
+puts "PONTUACAO"
+puts "#{nomes[0]} - #{numeros[2]}"
+
+puts "#{nomes}"
+
+puts nomes[0]
+puts nomes[1]
+puts nomes[2]
+
+puts nomes[-1]
+
+
+exemplo = [1,2,[4,5],6]
+#          0 1   2   3
+
+puts exemplo[0]
+puts exemplo[1]
+puts exemplo[3]
+
+puts exemplo[2]
+
+
+#Como é uma lista ordenada conseguimos acessar ela
+#Com o ruby funciona valores negativos , quando se coloca -1 por exemplo ele entende que você quer o último elemento da lista .
