4214915be8a25ca4393b9b26fe21e31c9d22581c
BeaPc <developer.beacandido@gmail.com>
2023-02-13 23:38:22 -0300

    feat: exercise 3 correction

---
+++ exercicios/aula-6-correcao/tres-has.rb
@@ -0,0 +1,9 @@
+exemplo = {
+  a: "cinquenta",
+  b: "dez",
+  c: [0,1,2,3,4,5,6,7,8,9]
+}
+
+puts "#{exemplo[:a]} - #{exemplo[:c][5]} #{exemplo[:c][0]}"
+puts "#{exemplo[:b]}- #{exemplo[:c][1]} #{exemplo[:c][0]}"
+
