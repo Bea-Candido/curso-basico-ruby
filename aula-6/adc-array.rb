9ed2c02a5492f41d30b29175446eef98746b6a7d
BeaPc <developer.beacandido@gmail.com>
2023-02-13 21:00:12 -0300

    feat: new class

---
+++ aula-6/adc-array.rb
@@ -0,0 +1,35 @@
+# 1
+exemplo = []
+puts exemplo[0] 
+
+exemplo[0]="Rafael"
+puts "#{exemplo}"
+
+exemplo[1] = 10
+exemplo[2] = "Roberto"
+puts "#{exemplo}"
+
+exemplo[0] = 50
+puts "#{exemplo}"
+
+# 2
+
+exemplo = [] 
+
+exemplo << "Rafael"
+exemplo << "Roberto"
+exemplo << 2 
+# a << sempre coloca no final da array
+
+puts "#{exemplo}"
+
+# 3
+
+exemplo = ["a","b"]
+
+exemplo << "c"
+exemplo << "d"
+
+puts "#{exemplo}"
+
+
