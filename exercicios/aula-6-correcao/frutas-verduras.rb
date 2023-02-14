87dc88dafcb18f63cace108601d838f90374c68a
BeaPc <developer.beacandido@gmail.com>
2023-02-13 23:00:28 -0300

    feat: exercise and correction

---
+++ exercicios/aula-6-correcao/frutas-verduras.rb
@@ -0,0 +1,8 @@
+lista = {frutas: [], verduras:[] } 
+
+lista[:frutas] << "banana"
+lista[:verduras] << "cenoura"
+lista[:frutas] << "uva"
+lista[:verduras] << "brocolis"
+
+puts lista
---
+++ exercicios/aula-6/frutas-verduras.rb
@@ -0,0 +1,13 @@
+mercado = {}
+
+mercado{:frutas} = []
+mercado{:verduras} = []
+
+mercado[:verduras] << cenoura 
+mercado[:verduras] << brocolis
+
+mercado[:frutas] << banana
+mercado[:frutas] << uva
+
+puts mercado 
+
