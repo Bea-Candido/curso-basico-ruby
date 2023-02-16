200b7032faf510d625a2a4250ecdff2a7bea241b
BeaPc <developer.beacandido@gmail.com>
2023-02-16 09:37:14 -0300

    feat: new exercise

---
+++ exercicios/aula-7/ex.3.rb
@@ -0,0 +1,19 @@
+# 1-
+# exemplo = {valor:9 , taxa:5}
+# exemplo = {valor:14 , taxa:14 }
+
+# if exemplo[:valor] > 15 or exemplo[:taxa] > 15
+#   puts "Muito alto"
+# elsif exemplo[:valor] >= 30 or exemplo[:taxa] >= 30
+#   puts"Soma muito alta"
+# end
+
+# 2-
+exemplo = {valor:15 , taxa:15}
+exemplo = {val:30 , tax:30}
+
+if exemplo[:valor] > 15 or exemplo[:taxa] > 15
+  puts "Muito alto"
+elsif exemplo[:valor] >= 30 or exemplo[:taxa] >= 30
+  puts"Soma muito alta"
+end
