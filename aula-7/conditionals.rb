5c4edc18bcc2a393c495ae372a20ec9c19381009
BeaPc <developer.beacandido@gmail.com>
2023-02-15 22:30:19 -0300

    feat: new class

---
+++ aula-7/conditionals.rb
@@ -0,0 +1,32 @@
+if true then 
+   puts "é verdadeiro"
+end 
+
+if true 
+   puts "é verdadeiro"
+end 
+
+exemplo = "Lucy"
+if exemplo == "Lucy"
+    puts "Hey Lucy"
+end
+
+# ==
+# != (diferente)
+# >
+# >=
+# <
+# <=
+
+exemplo = 5
+if exemplo != 1
+   puts "Não é 1"
+end
+
+exemplo = 11
+if exemplo <= 10
+  puts "Menor/ igual a 10"
+else  
+  puts "Maior que 10"
+end  
+
