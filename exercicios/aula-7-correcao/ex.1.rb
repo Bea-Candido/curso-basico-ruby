2802605cbcc4cabcac044a23f4199ef1bb7ec12a
BeaPc <developer.beacandido@gmail.com>
2023-02-16 08:47:25 -0300

    feat: exercise correction

---
+++ exercicios/aula-7-correcao/ex.1.rb
@@ -0,0 +1,12 @@
+# peso = 100
+# peso = 30
+
+peso = 50
+
+if peso >= 70
+   puts "Está acima do peso"
+elsif peso > 40 and peso < 70
+  puts "Esta no peso ideal"
+elsif peso <= 40
+  puts "Está abaixo do peso"     
+end   
