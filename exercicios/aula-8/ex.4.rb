14584e999ff742878949c4125ec6684fdaea32cd
BeaPc <developer.beacandido@gmail.com>
2023-02-20 23:00:19 -0300

    feat: .

--- exercicios/aula-8/ex.4.rb
+++ exercicios/aula-8/ex.4.rb
@@ -1,13 +1,13 @@
 agenda= [{nome:"Thalita",telefone:"5566"},{nome:"Maria",telefone:"9988"},{nome:"Barbara",telefone:"2299"}]
 procurar_por = "Barbara"
 
-agenda.each do |chave,valor|
-  if chave == "Thalita"
-    next
-  elsif  chave == "Maria"
-    next 
-  break 
-  end 
+
+agenda.each do |contato|
+  if contato[:nome] == procurar_por
+    puts "Here is the contact: Name:#{contato[:nome]} and telefone: #{contato[:telefone]}"
+    break
+  else
+    next  
+  end
 end 
 
-puts "#{chave},#{valor}"
